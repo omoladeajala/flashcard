4bc39fa526b5906a9901caa3f3176893daad401d
Omolade Ajala <omoladeajala2021@gmail.com>
2025-08-26 12:23:05 +0100

    Fix FlashCard component to toggle selection state and update class names for styling

--- src/App.js
+++ src/App.js
@@ -47,17 +47,20 @@ const questions = [
 function FlashCard({ questions }) {
   const [selectedId, setSelectedId] = useState(null);
 
-  function handleClick(id){
-  setSelectedId(id);
-};
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
 
   return (
     <div className="flashcards">
       {questions.map((question) => (
-        <div 
-        key={question.id}
-        onClick={() => handleClick(question.id)} 
-          className={question.id === selectedId ? "selected" : ""}>
+        <div
+          key={question.id}
+          onClick={() => handleClick(question.id)}
+          className={
+            "card" + (question.id === selectedId ? " selected" : "")
+          }
+        >
           <p>{question.id === selectedId ? question.answer : question.question}</p>
         </div>
       ))}
